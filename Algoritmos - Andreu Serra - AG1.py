1ee6c74201f253c3986c7abc26c6174f66efb15d
AndreuSerraSastre <71096497+AndreuSerraSastre@users.noreply.github.com>
2023-06-13 10:00:37 +0200

    Update Algoritmos - Andreu Serra - AG1.py

--- Algoritmos - Andreu Serra - AG1.py
+++ Algoritmos - Andreu Serra - AG1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def torres_hanoi(N, origen, destino, pivote):
   if N == 1:
     print(f"Mover bloque desde {origen} a {destino}")
@@ -8,3 +10,29 @@ def torres_hanoi(N, origen, destino, pivote):
   torres_hanoi(N-1, pivote, destino, origen)
 
 torres_hanoi(4, 1, 3, 2)
+
+def cambio_monedas(cantidad, sistema):
+  sistema.sort(reverse=True)
+  print(f"Sistema: {sistema}")
+  solucion = np.zeros(len(sistema), dtype=int)
+  valor_acumulado = 0
+  i = 0
+  for moneda in sistema:
+    monedas = int((cantidad - valor_acumulado)/moneda)
+    solucion[i] = monedas
+    valor_acumulado += monedas*moneda
+    if valor_acumulado == cantidad:
+      print(f"Solución: {solucion}")
+      return solucion
+    i += 1
+  print("No es posible hacer cambio exacto con las monedas proporcionadas.")
+  return None
+
+def cambio_monedas(cantidad, sistema):
+  sistema.sort(reverse=True)
+  solucion = [0] * len(sistema)
+  
+  for i, moneda in enumerate(sistema):
+    solucion[i], cantidad = divmod(cantidad, moneda)
+
+  return solucion
